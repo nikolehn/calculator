baf55e57623c93adcec667a8f4d9c9e165a4403b
nikolehn <nikole@riseup.net>
2014-09-07 23:07:38 -0600

    Added UI logic for operations of one operand.

--- web/calculator.js
+++ web/calculator.js
@@ -9,27 +9,73 @@ $(document).ready(function() {
         dot_used = false;
     }
 
+    function flash_input() {
+        $('#input').css('border-color', 'crimson');
+        setInterval(function() {
+            $('#input').css('border-color', 'white');
+        }, 2000);
+    }
+
+    function add_history(line) {
+        $('#history').append(
+            line + '<br/>'
+        );
+    }
+
     // Numbers
     $('button.num').click(function() {
         $('#input').append($(this).text());
     });
 
+    // One operand operations
+    $('button.op1').click(function() {
+
+        var operation = $(this).attr('id');
+        var operand = $('#input').text();
+
+        // Check the operand exists
+        if (!operand) {
+            flash_input();
+            return;
+        }
+
+        // Perfom call to server
+        $.ajax({
+            type: 'GET',
+            dataType: 'json',
+            url: '/calculator/api/calc/' +
+                operation + '/' +
+                operand,
+            success: function(data) {
+                add_history(operation + '(' + operand + ')');
+                dot_used = false;
+                $('#input').text(data.result);
+            },
+            error: function(xhr, opts, err) {
+                console.log(xhr.status);
+                console.log(err);
+                console.log(xhr.responseText);
+                console.log(xhr);
+                alert('Invalid entry!');
+                clear();
+            }
+        });
+    });
+
     // Dot
-    $('button.dot').click(function() {
+    $('#dot').click(function() {
         if (!dot_used) {
             $('#input').append('.');
             dot_used = true;
         }
     });
 
-    // Clean
-    $('button.clear').click(clear);
+    // Clear
+    $('#clear').click(clear);
 
     // Equal
-    $('button.eq').click(function() {
-        $('#history').append(
-            '\n' + $('#input').text()
-        );
+    $('#eq').click(function() {
+        add_history($('#input').text());
         clear();
     });
 });
