b1a75a992618d7a39fd4b4167a437fd6b3ac6244
nikolehn <nikole@riseup.net>
2014-09-07 23:06:20 -0600

    Added basic UI logic in client side script.

--- web/calculator.js
+++ web/calculator.js
@@ -1,5 +1,35 @@
 $(document).ready(function() {
+
+    // State
+    var dot_used = false;
+
+    // Helpers
+    function clear() {
+        $('#input').text('');
+        dot_used = false;
+    }
+
+    // Numbers
     $('button.num').click(function() {
-        $('pre#input').append($(this).text());
+        $('#input').append($(this).text());
+    });
+
+    // Dot
+    $('button.dot').click(function() {
+        if (!dot_used) {
+            $('#input').append('.');
+            dot_used = true;
+        }
+    });
+
+    // Clean
+    $('button.clear').click(clear);
+
+    // Equal
+    $('button.eq').click(function() {
+        $('#history').append(
+            '\n' + $('#input').text()
+        );
+        clear();
     });
 });
